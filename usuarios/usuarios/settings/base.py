7434c19be9f280b3a79bc09a73c7f2fd78169850
HansAlej0 <hanscalderon.estudiante@iser.edu.co>
2023-11-15 17:34:06 -0500

    Creacion Usuario

--- usuarios/usuarios/settings/base.py
+++ usuarios/usuarios/settings/base.py
@@ -45,7 +45,9 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-LOCAL_APPS=()
+LOCAL_APPS=(
+    'applications.users',
+)
 
 THIRD_PARTY_APPS=()
 
@@ -100,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL='users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
